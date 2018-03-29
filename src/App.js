9db6f04d049d246b8ec59f628167e7ab686b1a02
David Kim <davidkim310@gmail.com>
2018-03-29 13:44:29 -0700

    adds auth view and redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers'
+import LoginForm from './components/LoginForm';
+import ReduxThunk from 'redux-thunk';
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: "AIzaSyCVkBuPkDytFG8445avNz121zvddwRn5BM",
+            authDomain: "blandy-96cee.firebaseapp.com",
+            databaseURL: "https://blandy-96cee.firebaseio.com",
+            projectId: "blandy-96cee",
+            storageBucket: "",
+            messagingSenderId: "758719429926"
+          };
+
+        firebase.initializeApp(config);
+    }
     render() {
+        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+        
         return (
-            <View>
-                <Header headerText="HeaderText" />
-            </View>
+            <Provider store={store}>
+                <LoginForm />
+            </Provider>
         )
     }
 }
