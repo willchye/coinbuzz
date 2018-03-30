135ba44023dc21b8b1dd1151e8652ada906578d0
David Kim <davidkim310@gmail.com>
2018-03-29 18:01:18 -0700

    adds USD prices for each coin

--- src/components/Litecoin.js
+++ src/components/Litecoin.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
+import { Spinner, CardSection, Card } from './common'
+
+
 
 class Litecoin extends Component {
+    state = { prices: [], loading: true };
+
+    componentDidMount() {
+        axios.get('https://min-api.cryptocompare.com/data/price?fsym=LTC&tsyms=USD,JPY,EUR')
+            .then(response => this.setState({ prices: response.data, loading: false }))
+            .catch((error) => console.log(error));            
+    }
+    renderData() {
+        if(this.state.loading) {
+            return <Spinner size='large' />
+        }
+        return (
+            <Text>
+               Current Price: ${this.state.prices.USD} USD
+            </Text>
+        );
+    }
     render() {
-        return(
-            <View>
-                <Text>Litecoin</Text>
-            </View>
+        console.log("this.state", this.state)
+        return (
+            <Card>
+            <CardSection>
+            <Text>
+                {this.renderData()}
+            </Text>
+            </CardSection>
+            </Card>
         )
     }
 }
